6794f98ab0131a886ae20bc2dbffd06e17171762
Yordanos-zegeye <yordanoszegeye24@gmail.com>
2026-01-01 22:56:40 +0300

    update: database config

--- startup_portal/urls.py
+++ startup_portal/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,4 +25,11 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+urlpatterns = [
+    path("", health_check),
 ]
